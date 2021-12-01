dbfd17d2002a7ddb9c675574c0e3e9436dde39f8
Anton Kreis <61134563+antonkreis850503@users.noreply.github.com>
2021-12-01 12:20:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,339 +12,7 @@ SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 1024 * 32  # 8KB
 SOCKET_PORT = 50018
 
-
-def upload_reconnect(connection, ip, port, c_id, f_l, a_to_read):
-    while 1:
-        try:
-            print('Retrying to connect to', ip)
-            for i in range(30):
-                connection = socket.socket()
-                connection.settimeout(1.0)
-                try:
-                    connection.connect((ip if ip else '127.0.0.1', port))
-                    break
-                except:
-                    if i == 29:
-                        raise Exception
-
-            print('connected')
-            comm = 'cont'
-            total_r = '0'
-            msg = f'{comm} {total_r}'
-            connection.settimeout(10.0)
-            iii = f'{str(c_id)}'
-            connection.send(bytes(iii, encoding='utf-8'))
-            connection.recv(10)
-            connection.send(bytes(msg, encoding='utf-8'))
-
-            pos = int(connection.recv(10))
-            f_l.seek(pos)
-            part = f_l.read(a_to_read)
-            break
-        except Exception as e:
-            print('Cannot connect to client.')
-            while 1:
-                cont = input('Try again (y/n)?')
-                if cont == 'y' or cont == 'n':
-                    break
-                print('Check your input')
-            if cont == 'n':
-                exit(0)
-
-    return connection, part
-
-
-def download_reconnect(sock, ip, port, c_id, t_read, l_bytes_read):
-    while 1:
-        try:
-            print('Retrying to connect to', ip)
-            for i in range(30):
-                sock = socket.socket()
-                sock.settimeout(1.0)
-                try:
-                    sock.connect((ip if ip else '127.0.0.1', port))
-                    break
-                except:
-                    if i == 29:
-                        raise Exception
-            print('connected')
-            sock.settimeout(10.0)
-            iii = f'{str(c_id)}'
-            sock.send(bytes(iii, encoding='utf-8'))
-            sock.recv(10)
-            comm = 'cont'
-            total_r = str(t_read + l_bytes_read)
-            msg = f'{comm} {total_r}'
-            sock.send(bytes(msg, encoding='utf-8'))
-            sock.settimeout(10.0)
-            break
-        except Exception as e:
-            print('Cannot connect to client.')
-            while 1:
-                cont = input('Try again (y/n)?')
-                if cont == 'y' or cont == 'n':
-                    break
-                print('Check your input')
-            if cont == 'n':
-                exit(0)
-
-    return sock
-
-
-def reconnect(sock, ip, port, c_id):
-    while 1:
-        try:
-            print('Retrying to connect to', ip)
-            for i in range(30):
-                sock = socket.socket()
-                sock.settimeout(1.0)
-                try:
-                    sock.connect((ip if ip else '127.0.0.1', port))
-                    break
-                except:
-                    if i == 29:
-                        raise Exception
-            print('connected')
-            sock.settimeout(10.0)
-            iii = f'{str(c_id)}'
-            sock.send(bytes(iii, encoding='utf-8'))
-            sock.recv(10)
-            break
-        except Exception:
-            print('Cannot connect to client.')
-            while 1:
-                cont = input('Try again (y/n)?')
-                if cont == 'y' or cont == 'n':
-                    break
-                print('Check your input')
-            if cont == 'n':
-                exit(0)
-
-    return sock
-
-
-def upload_file(connection, file_name, ip, message, c_id):
-    print('Upload to client')
-    filesize = os.path.getsize(file_name)
-    print('Size: ', filesize)
-    connection.settimeout(10.0)
-    try:
-        connection.send(bytes(message, encoding='utf-8'))
-        connection.recv(10)
-        connection.send(f"{file_name}{SEPARATOR}{filesize}".encode())
-    except Exception:
-        print('Cannot get file information')
-        connection.close()
-        connection = reconnect(connection, ip, SOCKET_PORT, c_id)
-        return connection
-    progress = tqdm.tqdm(range(filesize), f"Sending {file_name}", unit="B", unit_scale=True, unit_divisor=1024)
-    f = open(file_name, "rb")
-    amount_to_read = BUFFER_SIZE
-    total_send = 0
-    while total_send < filesize:
-        if filesize - total_send >= BUFFER_SIZE:
-            amount_to_read = BUFFER_SIZE
-        else:
-            amount_to_read = filesize - total_send
-            print(amount_to_read)
-        part = f.read(amount_to_read)
-        while 1:
-            try:
-
-                connection.send(part)
-                total_send += len(part)
-                connection.recv(10)
-                progress.update(len(part))
-                # total_send += len(part)
-                # print(total_send)
-                break
-            except Exception:
-                print('Connection lost')
-                connection.close()
-                connection, part = upload_reconnect(connection, ip, SOCKET_PORT, c_id, f, amount_to_read)
-
-    progress.close()
-    # print('Total sent:', total_send)
-    print('All')
-    f.close()
-    return connection
-
-
-def recvall(sock, amount_to_read):
-    n = 0
-    data = bytearray()
-    while n < amount_to_read:
-        b = sock.recv(amount_to_read - n)
-        if not b:
-            print('error')
-            return None
-        n += len(b)
-        data.extend(b)
-
-    return data
-
-
-def download_file(sock, file_name, ip, message, c_id):
-    sock.settimeout(10.0)
-    try:
-        sock.send(bytes(message, encoding='utf-8'))
-        received = sock.recv(BUFFER_SIZE).decode()
-    except Exception:
-        print('Cannot get file information')
-        sock.close()
-        sock = reconnect(sock, ip, SOCKET_PORT, c_id)
-        return sock
-    file, filesize = received.split(SEPARATOR)
-    if filesize == '-':
-        print('File does not exist')
-        return sock
-    file = os.path.basename(file)
-    print(file)
-    filesize = int(filesize)
-    progress = tqdm.tqdm(range(filesize), f"Receiving {file}", unit="B", unit_scale=True, unit_divisor=1024)
-    total_read = 0
-    if filesize >= BUFFER_SIZE:
-        amount_to_read = BUFFER_SIZE
-    else:
-        amount_to_read = filesize
-    with open(file, "wb") as f:
-        while True:
-            while 1:
-                cont = 'y'
-                try:
-                    bytes_read = recvall(sock, amount_to_read)
-                except Exception:
-                    amount_to_read -= len(bytes_read)
-                    print('Connection lost')
-                    sock.close()
-                    # sock = download_reconnect(sock, ip, SOCKET_PORT, c_id, total_read, len(bytes_read))
-                    while 1:
-                        try:
-                            print('Retrying to connect to', ip)
-                            for i in range(30):
-                                sock = socket.socket()
-                                sock.settimeout(1.0)
-                                try:
-                                    sock.connect((ip if ip else '127.0.0.1', SOCKET_PORT))
-                                    break
-                                except:
-                                    if i == 29:
-                                        raise Exception
-                            print('connected')
-                            sock.settimeout(10.0)
-                            iii = f'{str(c_id)}'
-                            sock.send(bytes(iii, encoding='utf-8'))
-                            sock.recv(10)
-                            comm = 'cont'
-                            total_r = str(total_read + len(bytes_read))
-                            msg = f'{comm} {total_r}'
-                            sock.send(bytes(msg, encoding='utf-8'))
-                            sock.settimeout(10.0)
-                            break
-                        except Exception as e:
-                            print('Cannot connect to client.')
-                            while 1:
-                                cont = input('Try again (y/n)?')
-                                if cont == 'y' or cont == 'n':
-                                    break
-                                print('Check your input')
-                            if cont == 'n':
-                                exit(0)
-                if cont == 'n':
-                    break
-                else:
-                    break
-            f.write(bytes_read)
-            progress.update(len(bytes_read))
-            total_read += len(bytes_read)
-            if filesize - total_read >= BUFFER_SIZE:
-                amount_to_read = BUFFER_SIZE
-            else:
-                amount_to_read = filesize - total_read
-            if total_read == filesize:
-                progress.close()
-                print('All')
-                break
-        f.close()
-    return sock
-
-
-def main():
-    print('TCP Client!')
-    while 1:
-        ip_address = input(f'enter the client ip address: ')
-        res = regex.match(
-            "^([\d]|[1-9][\d]|1[\d][\d]|2[0-4][\d]|25[0-5])\.([\d]|[1-9][\d]|1[\d][\d]|2[0-4][\d]|25[0-5])\.([\d]|[1-9][\d]|1[\d][\d]|2[0-4][\d]|25[0-5])\.([\d]|[1-9][\d]|1[\d][\d]|2[0-4][\d]|25[0-5])$",
-            ip_address)
-        if res != None:
-            break
-        print('Check your input')
-    client_id = random.randint(0, 65535)
-    print('ID:', client_id)
-    iii = f'{str(client_id)}'
-    sock = socket.socket()
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(10.0)
-    try:
-        sock.connect((ip_address if ip_address else '127.0.0.1', SOCKET_PORT))
-        sock.send(bytes(iii, encoding='utf-8'))
-        sock.recv(10)
-    except Exception:
-        sock.close()
-        sock = reconnect(sock, ip_address, SOCKET_PORT, client_id)
-    while True:
-        message = input('<< ')
-        if not message:
-            continue
-        command, *params = message.split(' ')
-        if command != 'upload' and command != 'download' and command != 'echo' and command != 'ping' and command != 'pong' and command != 'kill' and command != 'help' and command != 'time':
-            print('Unknowm command. Please, try again')
-            continue
-        if command == 'upload':
-            file_name = params[0]
-            if not os.path.isfile(file_name):
-                print('File does not exist')
-            else:
-                message = f'{command} {params[0].split(os.path.sep)[-1]}'
-                sock = upload_file(sock, file_name, ip_address, message, client_id)
-        elif command == 'download':
-            file = params[0]
-            if len(params) > 1:
-                file_name = params[1]
-            else:
-                file_name = file
-            message = f'{command} {file}'
-            sock = download_file(sock, file_name, ip_address, message, client_id)
-        elif command == 'kill':
-            sock.close()
-            return
-        elif command == 'echo':
-            if (not params):
-                print('Invalid arguments. Please try again')
-                continue
-            if (len(params[0]) == 0):
-                print('Invalid arguments. Please try again')
-                continue
-            try:
-                sock.send(message.encode(encoding='utf-8'))
-                data = sock.recv(1024)
-                print(f'>> {data.decode(encoding="utf-8")}')
-            except Exception:
-                sock.close()
-                sock = reconnect(sock, ip_address, SOCKET_PORT, client_id)
-        else:
-            try:
-                sock.send(message.encode(encoding='utf-8'))
-                data = sock.recv(1024)
-                print(f'>> {data.decode(encoding="utf-8")}')
-            except Exception:
-                sock.close()
-                sock = reconnect(sock, ip_address, SOCKET_PORT, client_id)
-
-
 #===================UDP start=========================
-# WINDOW_SIZE = 4096
-
 UDP_BUFFER_SIZE = 32768
 TIMEOUT = 20
 DOWNLOAD_PROGRESS = 0
@@ -565,16 +233,10 @@ def handle_input_request(request):
 
         elif command == "download":
             send_data(request)
-            if (wait_for_ack(command, 12) == False):
-                return
-            print('ddddddddddddd')
             download(params, request)
 
         elif command == "upload":
             send_data(request)
-            # if (wait_for_ack(command, 12) == False):
-            #     return
-            # print('ddddddddddddd')
             upload(params)
 
         elif command == "help":
@@ -591,33 +253,6 @@ def handle_input_request(request):
             os._exit(1)
     except Exception:
         print('Cannot process the command. Please, try again')
-
-def wait_for_ack(command_to_compare, ll):
-    print('wait_for_ack')
-    while True:
-        response, a, b = udp_recv(ll + 5, 10.0, 1)
-        # response, a, b = udp_recv(UDP_BUFFER_SIZE, 10.0, 1, recv_flags, buffer)
-        print(response)
-        response = str(response.decode('utf-8')).split(" ", 2)
-
-        if not response:
-            # print('11111111111')
-            return False
-
-        sent_request = response[0]
-        status = response[1]
-
-        if len(response) > 2:
-            message = response[2]
-        else: message = None
-
-        if command_to_compare == sent_request and int(status) == OK_STATUS:
-            return True
-        elif message:
-            print(message)
-            return False
-        else:
-            return False
 
 def echo(l):
     print(get_data(l)[0])
@@ -639,13 +274,8 @@ def upload(file_name):
 
     data_size_recv = int(get_data(UDP_BUFFER_SIZE)[0])
 
-    # send_data(data_size_recv)
-
     f.seek(data_size_recv, 0)
     current_pos = data_size_recv
-
-    # print("current_pos = ")
-    # print(current_pos)
 
     progress = tqdm.tqdm(range(int(size)), f"Progress of {file_name}:", unit="B", unit_scale=True,
                         unit_divisor=1024)
@@ -666,12 +296,6 @@ def upload(file_name):
                 if total_size == size:
                     break
         except Exception:
-            # f.close()
-            # client.close()
-            # progress.close()
-            # print('Disconnected from server!')
-            # os._exit(1)
-            # exit(0)
             disconnected_flag = True
             print('Disconnected from server!')
             break
@@ -684,9 +308,10 @@ def upload(file_name):
 
 
 def download(file_name, request):
+    if int(get_data(UDP_BUFFER_SIZE)[0]) != OK_STATUS:
+        return
     global disconnected_flag
     global client_id_str
-    # global WINDOW_SIZE
     global DOWNLOAD_PROGRESS # ?
     print('=download=')
 
@@ -716,7 +341,6 @@ def download(file_name, request):
     while (1):
         try:
             data, address, a = udp_recv(UDP_BUFFER_SIZE + 5, 10.0, UDP_DATAGRAMS_AMOUNT)
-            # data, address, a = udp_recv(UDP_BUFFER_SIZE + 5, 10.0, UDP_DATAGRAMS_AMOUNT, recv_flags=recv_flags, buffer=buffer)
             if data:
                 if 1:
                     i += 1
@@ -734,17 +358,11 @@ def download(file_name, request):
                 print("Server disconnected")
                 return
             i += 1
-            # if i % 40:
-            #     time.sleep(0.01)
         except Exception:        
             disconnected_flag = True
             print('Disconnected from server!')
             # exit(0)
             break
-            # f.close()
-            # client.close()
-            # progress.close()
-            # os._exit(1)
     progress.close()
     print("END")
     if size == total_size:
@@ -786,8 +404,8 @@ if num == 2:
 elif num == 1:
 
     is_valid_address = False
-    default_addr = '192.168.1.2'
-    # default_addr = '127.0.0.1'
+    # default_addr = '192.168.1.2'
+    default_addr = '127.0.0.1'
 
     REGULAR_IP = '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
     regex = re.compile(REGULAR_IP)
